2e0092fc951b47d60e2a329a9ddc1db6adeab21f
Yukta <yuktaravindrathakur2006@gmail.com>
2026-07-14 14:42:22 +0530

    fix: revert firebase config to hardcoded values for deployment

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCy2LN94AyjLHCV3mjWOu5nuR71AVDkrDc",
+  authDomain: "nexthire-c1d7a.firebaseapp.com",
+  projectId: "nexthire-c1d7a",
+  storageBucket: "nexthire-c1d7a.firebasestorage.app",
+  messagingSenderId: "775844734769",
+  appId: "1:775844734769:web:e08eac8607bcaeecf57210",
+  measurementId: "G-EWGHBVPQW9",
 };
 
 // Initialize Firebase for SSR / Client-side hydration
